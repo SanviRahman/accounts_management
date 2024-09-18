5e846fd1cd141c61a41658d6444c8414cb4db48e
SanviRahman <sheikh15-3700@diu.edu.bd>
2024-09-18 06:01:29 +0600

    first commit

--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
 
-def reginstration(requenst):
-    form=UserCreationForm()
+def reginstration(request):
+    if request.method=="POST":
+        form=UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('register')
+    else:
+        form=UserCreationForm()
+
     context={
         'form':form
     }   
-    return render(requenst,'accounts/register.html',context)
+    return render(request,'accounts/register.html',context)
